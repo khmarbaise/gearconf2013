2669b16a33f38b4a719b6f1219c1c8d742e79357
Karl Heinz Marbaise <info@soebes.de>
2013-10-09 19:29:39 +0200

    - Added bad build with profile.

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 find . -type f -name ".project" | xargs rm -f
 find . -type f -name ".classpath" | xargs rm -f
-find . -type d -name ".setting" | xargs rm -fr
+find . -type d -name ".settings" | xargs rm -fr
